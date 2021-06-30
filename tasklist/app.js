7aa7d199a52ce54ed59a2a20f1d1f4283948ffc5
Sattvik Palta <sattvikpalta@gmail.com>
2021-06-30 18:28:00 +0530

    Tasklist

---
+++ tasklist/app.js
@@ -0,0 +1,10 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const tasksList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+
+// Load all event listeners
+loadEventListeners();
+
