60f8a96e22c15cbf9698c7713dbe4f7fcfc4d2e9
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-03 14:54:08 +0530

    Number Guesser game

---
+++ number-guesser/app.js
@@ -0,0 +1,8 @@
+/*
+GAME Function
+- Player must guess a number between min and max
+- Player gets a certain amount of guesses
+- Notify the player of guesses remaining
+- Notify the player of the correct answer if he/she looses
+- Let player choose to play again
+*/
