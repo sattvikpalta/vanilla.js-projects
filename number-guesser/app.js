1f8389b330a599d7e8a8ad9a46a5373a7dc2861f
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-03 16:05:33 +0530

    Number Guesser game

--- number-guesser/app.js
+++ number-guesser/app.js
@@ -6,3 +6,48 @@ GAME Function
 - Notify the player of the correct answer if he/she looses
 - Let player choose to play again
 */
+
+// Game values
+let min = 1;
+let max = 10;
+let winningNum = 2;
+let guessesLeft = 3;
+
+// UI elements
+const game = document.getElementById('game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function () {
+  let guess = parseInt(guessInput.value);
+
+  // Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if (guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct, you win!`, 'green');
+  }
+
+});
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
