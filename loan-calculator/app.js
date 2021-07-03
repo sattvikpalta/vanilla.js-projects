b4f4bc264ee31bc9487c7521aa9477dc5fccf5c3
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-03 13:56:48 +0530

    Loan Calculator

--- loan-calculator/app.js
+++ loan-calculator/app.js
@@ -1,5 +1,16 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader as Calculate is clicked
+  document.getElementById('loading').style.display = 'block';
+
+  // Show for 1 sec or 1000ms
+  setTimeout(calculateResults, 1000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
 function calculateResults(e) {
@@ -26,15 +37,26 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatePayments).toFixed(2);
     totalInterest.value = ((monthly * calculatePayments) - principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
@@ -51,8 +73,8 @@ function showError(error) {
   // Insert error above heading
   card.insertBefore(errorDiv, heading);
 
-  // Clear error after 3 sec or 3000 ms
-  setTimeout(clearError, 3000);
+  // Clear error after 2 sec or 2000 ms
+  setTimeout(clearError, 2000);
 }
 
 // Clear Error
