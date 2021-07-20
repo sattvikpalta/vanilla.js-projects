36ca7b7ba5e179babd9a7dbbf26d2240b00943be
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-20 10:54:22 +0530

    Fetch and display repos

--- github-finder/github.js
+++ github-finder/github.js
@@ -1,16 +1,22 @@
 class GitHub {
   constructor() {
-    this.client_id = 'bbb02db81f320624f45b'
-    this.client_secret = 'c6a3a4dc2b038d9696919df0cf6300ea804660a5'
+    this.client_id = 'bbb02db81f320624f45b';
+    this.client_secret = 'c6a3a4dc2b038d9696919df0cf6300ea804660a5';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 
